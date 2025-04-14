28aefba4eb5d8aafddc7e4f0ad3363289a1e1338
Tharusha Jayawardane <tharushajayawardane05@gmail.com>
2025-04-14 18:22:36 +0530

    Update Hero.tsx

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -92,8 +92,8 @@ const Hero: React.FC = () => {
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="22"
-                    height="22"
+                    width="18"
+                    height="18"
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
