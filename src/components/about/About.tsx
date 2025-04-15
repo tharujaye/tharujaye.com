cd77676ab8ae5d739361459aedd77a0cf1a42619
Tharusha Jayawardane <tharushajayawardane05@gmail.com>
2025-04-15 18:53:17 +0530

    about me image v2

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -13,7 +13,7 @@ const About: React.FC = () => {
           <AnimatedSection animation="fade-in-right" className="relative">
             <div className="relative z-10">
               <img 
-                src="/portfolioaboutcollage.jpg" 
+                src="/aboutme.jpg" 
                 alt="About me" 
                 className="rounded-2xl w-full h-auto shadow-lg"
               />
