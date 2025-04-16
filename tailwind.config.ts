5d17dccf66ab6dc28fb38f6d0dc177eda951a43f
Tharusha Jayawardane <tharushajayawardane05@gmail.com>
2025-04-16 17:29:12 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,10 @@ export default {
 			center: true,
 			padding: '2rem',
 			screens: {
+				sm: '640px',
+      			md: '768px',
+      			lg: '1024px',
+      			xl: '1280px',
 				'2xl': '1400px'
 			}
 		},
@@ -134,5 +138,5 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [import("tailwindcss-animate")],
 } satisfies Config;
