a0be80d04c2e08fe1c4cde24bb22a003e81a536c
Tharusha Jayawardane <tharushajayawardane05@gmail.com>
2025-04-19 01:28:47 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,11 @@ import type { Config } from "tailwindcss";
 export default {
 	darkMode: ["class"],
 	content: [
+		"./index.html",
 		"./pages/**/*.{ts,tsx}",
 		"./components/**/*.{ts,tsx}",
 		"./app/**/*.{ts,tsx}",
-		"./src/**/*.{ts,tsx}",
+		"./src/**/*.{js, jsx, ts, tsx}",
 	],
 	prefix: "",
 	theme: {
